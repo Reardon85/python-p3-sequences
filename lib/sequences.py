00558099f2435713ff38b4342d6cf84c718f4499
Brian Reardon <brian.d.reardon@gmail.com>
2023-04-10 00:33:57 -0400

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    num1, num2 = 0, 1
+    i = 0
+    fib = list()
+    while (i < length):
+        fib.append(num1)
+        num1, num2 = num2, num1 + num2
+        i += 1
+    print(fib)
